17596150d281f8ba376e8f1f5824c29ec7503cc6
NoCodeNoLife <1812532016@qq.com>
2015-11-30 15:36:33 +0800

    add

---
+++ Javascript/library.js
@@ -0,0 +1,27 @@
+/**
+ * Created by zhaoshuai on 2015/11/30.
+ */
+function getId(id){
+    return document.getElementById(id);
+};
+
+function getByClass(oParent,sClass){
+    if (oParent.getElementsByClassName) {
+        return oParent.getElementsByClassName(sClass);
+    } else {  //IE 8 7 6
+        var arr = [];
+        var reg = new RegExp('\\b' + sClass + '\\b');
+        var aEle = oParent.getElementsByTagName('*');
+        for (var i = 0; i < aEle.length; i++) {
+            if (reg.test(aEle[i].className)) {
+                arr.push(aEle[i]);
+            }
+        }
+        return arr;
+    }
+};
+
+function getStyle(obj,name){
+    return (obj.currentStyle || getComputedStyle(obj,false))[name];
+}
+
