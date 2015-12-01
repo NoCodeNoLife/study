e2a3ceef644ce2b118300475660fdda19b5e3187
NoCodeNoLife <1812532016@qq.com>
2015-12-01 10:31:55 +0800

    modify

--- Javascript/getid.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by zhaoyi on 2015/11/28.
- */
-function $(id){
-    return document.getElementById(id);
-}
--- Javascript/libs/library.js
+++ Javascript/libs/library.js
@@ -35,4 +35,29 @@ function addReady(fn){
             }
         };
     }
-}
+};
+
+function hasClass(obj,sClass){
+    var reg=new RegExp('\\b'+sClass+'\\b');
+    return reg.test(obj.className);
+};
+
+function addClass(obj,sClass){
+    if(obj.className){
+        if(!hasClass(obj,sClass)){
+            obj.className+=' '+sClass;
+        }
+    }else{
+        obj.className=sClass;
+    }
+};
+
+function removeClass(obj,sClass){
+    var reg=new RegExp('\\b'+sClass+'\\b','g');
+    if(hasClass(obj,sClass)){
+        obj.className=obj.className.replace(reg,'').replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
+    }
+};
+
+
+
