6b2f47d33d48cbb09820e9354b2e38b4f99d5d70
NoCodeNoLife <1812532016@qq.com>
2015-12-03 10:55:28 +0800

    add

--- Javascript/libs/library.js
+++ Javascript/libs/library.js
@@ -1,10 +1,12 @@
 /**
  * Created by zhaoshuai on 2015/11/30.
  */
+/*获取ID*/
 function $(id){
     return document.getElementById(id);
-};
+};/*获取ID*/
 
+/*获取className,兼容IE6-8*/
 function getByClass(oParent,sClass){
     if (oParent.getElementsByClassName) {
         return oParent.getElementsByClassName(sClass);
@@ -19,12 +21,9 @@ function getByClass(oParent,sClass){
         }
         return arr;
     }
-};
+};/*获取className,兼容IE6-8*/
 
-function getStyle(obj,name){
-    return (obj.currentStyle || getComputedStyle(obj,false))[name];
-};
-
+/*代替window.onload，加载速度更快*/
 function addReady(fn){
     if(document.addEventListener){
         document.addEventListener('DOMContentLoaded',fn,false);
@@ -35,13 +34,15 @@ function addReady(fn){
             }
         };
     }
-};
+};/*代替window.onload，加载速度更快*/
 
+/*包含className*/
 function hasClass(obj,sClass){
     var reg=new RegExp('\\b'+sClass+'\\b');
     return reg.test(obj.className);
-};
+};/*包含className*/
 
+/*添加className*/
 function addClass(obj,sClass){
     if(obj.className){
         if(!hasClass(obj,sClass)){
@@ -50,14 +51,81 @@ function addClass(obj,sClass){
     }else{
         obj.className=sClass;
     }
-};
+};/*添加className*/
 
+/*删除className*/
 function removeClass(obj,sClass){
     var reg=new RegExp('\\b'+sClass+'\\b','g');
     if(hasClass(obj,sClass)){
         obj.className=obj.className.replace(reg,'').replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
     }
-};
+};/*删除className*/
 
-
+/*获取行间样式和move框架*/
+function getStyle(obj,name){
+    return(obj.currentStyel || getComputedStyle(obj,false))[name];
+}
+//move框架
+function move(obj,json,options){
+    options=options||{};
+    options.time=options.time||1000;
+    options.type=options.type||'linaer';
+    var n=0;
+    var count=Math.round(options.time/30);
+    var dis={};
+    var start={};
+    for(var name in json){
+        if(name=='opacity'){
+            start[name]=parseInt(getStyle(obj,name));
+        }else{
+            start[name]=parseFloat(getStyle(obj,name));
+        }
+        dis[name]=json[name]-start[name];
+        if(isNaN(start[name])){
+            switch(name){
+                case 'left':
+                    start[name]=obj.offsetLeft;
+                    break;
+                case 'top':
+                    start[name]=obj.offsetTop;
+                    break;
+                case 'width':
+                    start[name]=obj.offsetWidth;
+                    break;
+                case 'height':
+                    start[name]=obj.offsetHeight;
+                    break;
+            }
+        }
+    }
+    clearInterval(obj.timer);
+    obj.timer=setInterval(function(){
+        n++;
+        for(var name in json){
+            switch(options.type){
+                case 'linaer':
+                    var cur=start[name]+dis[name]*n/count;
+                    break;
+                case 'ease-in':
+                    var a=n/count;
+                    var cur=start[name]+dis[name]*a*a*a;
+                    break;
+                case 'ease-out':
+                    var a=(1-n/count);
+                    var cur=start[name]+dis[name]*(1-a*a*a);
+                    break;
+            }
+            if(name=='opacity'){
+                obj.style.opacity=cur;
+                obj.style.fliter='alpha(opacity:"+cur*100+")';
+            }else{
+                obj.style[name]=cur+'px';
+            }
+        }
+        if(n==count){
+            clearInterval(obj.timer);
+            options.fn && options.fn();
+        }
+    },30);
+};/*获取行间样式和move框架*/
 
